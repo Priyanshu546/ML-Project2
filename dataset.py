9c69c1bedb0306387bd75c52f9fa19aca6f4f6c8
Priyanshu546 <146194215+Priyanshu546@users.noreply.github.com>
2024-04-10 03:44:20 +0530

    Create dataset.py

---
+++ dataset.py
@@ -0,0 +1,33 @@
+microsoft = pd.read_csv('MicrosoftStock.csv') 
+print(microsoft.head()) 
+microsoft.shape
+microsoft.info()
+microsoft.describe()
+plt.plot(microsoft['date'], 
+		microsoft['open'], 
+		color="blue", 
+		label="open") 
+plt.plot(microsoft['date'], 
+		microsoft['close'], 
+		color="green", 
+		label="close") 
+plt.title("Microsoft Open-Close Stock") 
+plt.legend() 
+plt.plot(microsoft['date'], 
+		microsoft['volume']) 
+plt.show()
+sns.heatmap(microsoft.corr(), 
+			annot=True, 
+			cbar=False) 
+plt.show() 
+microsoft['date'] = pd.to_datetime(microsoft['date']) 
+prediction = microsoft.loc[(microsoft['date'] 
+							> datetime(2013, 1, 1)) 
+							& (microsoft['date'] 
+							< datetime(2018, 1, 1))] 
+
+plt.figure(figsize=(10, 10)) 
+plt.plot(microsoft['date'], microsoft['close']) 
+plt.xlabel("Date") 
+plt.ylabel("Close") 
+plt.title("Microsoft Stock Prices")
